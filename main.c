506e5f6f2e3e2eaa7fa2b19ca41dd3a70f33ba2e
jang-seoyun <rachel2343@naver.com>
2025-11-06 16:04:27 +0900

    Update main.c

--- main.c
+++ main.c
@@ -7,14 +7,26 @@ int main(int argc, char *argv[])
 {
 	char input[100];
 	FILE* fp;
+	char c;
 	
 	//fopen
-	fp=fopen("sample.txt","w");
-	
-	//fprintf 
-	printf("input a word:");
-	scanf("%s", input);
-	fprintf(fp, "%s\n", input);
+	fp = fopen("sample.txt","r");
+	if (fp==NULL)
+	{
+		printf("Failed to open file!\n");
+		return -1;
+	}
+	#if 0
+	while( (c = fgetc(fp)) != EOF) 
+	{
+		putchar(c);
+	}
+	#else
+	while ( fgets(input, 100, fp) >0 )
+	{
+		printf("%s", input);
+	}
+	#endif
 	
 	//fclose
 	fclose(fp);
